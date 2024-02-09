27b2bd6eaeab548a2938b0f99fe76083ff560b38
Andrés <lopez.andresrigoberto@gmail.com>
2024-02-08 19:53:52 -0800

    edit: name

--- ecomsite/shop/models.py
+++ ecomsite/shop/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
-class Products(models.Model):
+class Product(models.Model):
     title = models.CharField(max_length=100)
     price = models.FloatField()
     discount_price = models.FloatField()
     category = models.CharField(max_length=100)
     description = models.TextField()
     image = models.CharField(max_length=500)
-    
+
     def __str__(self):
         return self.title
