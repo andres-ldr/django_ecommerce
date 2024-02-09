aebd0ae1056340160621e453008dab2f23a4d471
Andrés <lopez.andresrigoberto@gmail.com>
2024-02-08 22:36:24 -0800

    add: pagination

--- ecomsite/shop/views.py
+++ ecomsite/shop/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
 from .models import Product
@@ -7,7 +8,13 @@ from .models import Product
 
 def index(request):
     products = Product.objects.all()
+    # Searching
     item_name = request.GET.get("item_name")
     if item_name != "" and item_name is not None:
         products = products.filter(title__icontains=item_name)
+
+    # Pagination
+    paginator = Paginator(products, 4)
+    page = request.GET.get("page")
+    products = paginator.get_page(page)
     return render(request, "shop/index.html", {"products": products})
