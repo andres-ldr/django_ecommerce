349e2bf03cd11d39235528c09aaefb93dc2e6426
Andrés <lopez.andresrigoberto@gmail.com>
2024-02-08 22:06:42 -0800

    add: Search feature

--- ecomsite/shop/views.py
+++ ecomsite/shop/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+
 from .models import Product
+
 # Create your views here.
+
 
 def index(request):
     products = Product.objects.all()
-    return render(request, 'shop/index.html', {'products': products})
+    item_name = request.GET.get("item_name")
+    if item_name != "" and item_name is not None:
+        products = products.filter(title__icontains=item_name)
+    return render(request, "shop/index.html", {"products": products})
